fc076e6ca5917969bb37e546d895396151d503ae
Kay <kard@hotmail.co.uk>
2022-05-09 21:09:01 +0100

    Now asks for hobbies and height

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,17 +1,26 @@
 #we invite the user to create an array of students
 def input_students
-  puts "Please enter the names of the students."
+  puts "Please enter the details of the students."
   puts "To finish, just hit return twice"
 #create an empty array of students
   students = []
 #ask for the name
+  puts "Name?"
   name = gets.chomp
 #this will enter the students name as long as they did not enter an empty name
   while !name.empty? do
-    #create a hash with the name and cohort and add this to the array
-    students << {name: name, cohort: :november}
+    #ask for further details
+    puts "Hobbies?"
+    hobbies = gets.chomp
+      if hobbies.empty? then hobbies = "<no hobbies listed>" end
+    puts "Height?"
+    height = gets.chomp
+      if height.empty? then height = "<no height listed>" end
+    #create a hash with the details and add this to the array
+    students << {name: name, cohort: :november, hobbies: hobbies, height: height}
     puts "Now we have #{students.count} students"
     
+    puts "Now for the next student. Name?"
     name = gets.chomp
   end
 
@@ -28,6 +37,8 @@ def print(students)
   counter = 0
     while students.length > counter do
       puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+      puts "Hobbies: #{students[counter][:hobbies]}"
+      puts "Height: #{students[counter][:height]}"
       counter += 1
     end
 end
